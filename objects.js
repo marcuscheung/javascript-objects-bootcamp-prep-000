8a78bee2da00c1178d53ffe613e1801caa3c331a
Marcus Cheung <myc2131@tc.columbia.edu>
2018-04-21 04:57:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = {
   'My Bloody Valentine': 'Sometimes'};
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist.Phil Ochs] = "Here's to the State of Mississippi"
+  playlist.Phil Ochs = "Here's to the State of Mississippi"
   return updatePlaylist 
 }
